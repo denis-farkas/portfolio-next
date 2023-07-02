448690941cb504d1d5a1be8a75e21f6e3bd21367
denis farkas <dfarkas960@gmail.com>
2023-07-02 18:55:22 +0200

    seed

--- prisma/migrations/20221226162730_projets/migration.sql
+++ prisma/migrations/20221226162730_projets/migration.sql
@@ -16,3 +16,13 @@ CREATE TABLE `Project` (
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+CREATE TABLE `Contact` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NOT NULL,
+    `message` TEXT NOT NULL,
+    `date` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
--- prisma/migrations/20221226171806_project/migration.sql
+++
@@ -1,5 +0,0 @@
--- AlterTable
-ALTER TABLE `project` MODIFY `content` TEXT NOT NULL,
-    MODIFY `created` VARCHAR(191) NOT NULL,
-    MODIFY `frontend` TEXT NOT NULL,
-    MODIFY `backend` TEXT NOT NULL;
